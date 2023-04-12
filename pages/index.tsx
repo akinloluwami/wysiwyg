fe6eaf1f46f5c8ee79446dc9b6a8a7c06cc846c4
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-12 17:29:45 +0100

    ??

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Index = () => {
   return (
-    <div className="flex  justify-center">
+    <div className="flex flex-col w-[50%] mx-auto">
       <input
         type="text"
         placeholder="Post title"
-        className="mt-10 w-[550px] outline-none border-none text-4xl"
+        className="mt-10 w-full outline-none border-none text-4xl"
       />
       <Editor />
     </div>
