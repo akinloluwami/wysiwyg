4cda23b66a566724d72ffe70d739cdffd336058c
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-12 17:33:23 +0100

    base

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 
 const Editor = () => {
-  return <div className="mt-5">Editor</div>;
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World!</p>",
+    onUpdate: ({ editor }) => {
+      console.log(editor.getHTML());
+    },
+  });
+
+  return (
+    <div className="mt-5">
+      <EditorContent editor={editor} />
+    </div>
+  );
 };
 
 export default Editor;
