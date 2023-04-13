ceae98a5b5af4f32d7af09f8f4f71305e62450f6
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 01:29:10 +0100

    removed some tools

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -100,15 +100,6 @@ const ToolBar = ({ editor }: any) => {
     switch (format) {
       case "paragraph":
         editor.chain().focus().setParagraph().run();
-      case "bold":
-        editor.chain().focus().toggleBold().run();
-        break;
-      case "italic":
-        editor.chain().focus().toggleItalic().run();
-        break;
-      case "strike":
-        editor.chain().focus().toggleStrike().run();
-        break;
       case "heading1":
         editor.chain().focus().setHeading({ level: 1 }).run();
         break;
@@ -117,12 +108,6 @@ const ToolBar = ({ editor }: any) => {
         break;
       case "heading3":
         editor.chain().focus().setHeading({ level: 3 }).run();
-        break;
-      case "bulletList":
-        editor.chain().focus().toggleBulletList().run();
-        break;
-      case "orderedList":
-        editor.chain().focus().toggleOrderedList().run();
         break;
       default:
         break;
@@ -133,14 +118,9 @@ const ToolBar = ({ editor }: any) => {
     <div className="mb-10 flex gap-3">
       <select onChange={handleFormatSelect}>
         <option value="paragraph">Paragraph</option>
-        <option value="bold">Bold</option>
-        <option value="italic">Italic</option>
-        <option value="strike">Strike</option>
         <option value="heading1">Heading 1</option>
         <option value="heading2">Heading 2</option>
         <option value="heading3">Heading 3</option>
-        <option value="bulletList">Bullet List</option>
-        <option value="orderedList">Ordered List</option>
       </select>
       <div className="flex gap-3">
         {toolbarButtons.map((button, i) => (
