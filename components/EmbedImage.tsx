8de383839da28aae8dd3da1c6fded253d338aaa1
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 03:21:43 +0100

    stylings

--- components/EmbedImage.tsx
+++ components/EmbedImage.tsx
@@ -12,12 +12,23 @@ const EmbedImage = () => {
 
   return (
     <Modal isOpen={isOpen} onClose={handleCloseModal}>
-      <div className="bg-slate-200 w-[550px]">
+      <div className="bg-white w-[550px] p-5">
         <div className="flex justify-between items-center p-4">
           <h3>Upload Image</h3>
           <button onClick={handleCloseModal}>
             <MdClose />
           </button>
+        </div>
+        <div className="w-full h-[150px] border-dashed border-green-400 border-2 flex items-center justify-center">
+          <button className="border border-green-400 p-2 bg-gray-200">
+            Upload from device
+          </button>
+        </div>
+        <div className="flex gap-3 mt-5">
+          <button className="bg-green-400 py-2 px-8 rounded-sm text-white">
+            Embed
+          </button>
+          <button className="py-2 px-8 rounded-sm bg-gray-200">Cancel</button>
         </div>
       </div>
     </Modal>
