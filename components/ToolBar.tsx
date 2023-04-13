0615c09f96a8725db73a18add4609498914f6153
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 01:46:24 +0100

    _

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -20,19 +20,16 @@ const ToolBar = ({ editor }: any) => {
     const previousUrl = editor.getAttributes("link").href;
     const url = window.prompt("URL", previousUrl);
 
-    // cancelled
     if (url === null) {
       return;
     }
 
-    // empty
     if (url === "") {
       editor.chain().focus().extendMarkRange("link").unsetLink().run();
 
       return;
     }
 
-    // update link
     editor.chain().focus().extendMarkRange("link").setLink({ href: url }).run();
   }, [editor]);
 
