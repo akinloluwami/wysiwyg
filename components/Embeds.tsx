0df471dbd9948aff5eaf7f300d81b71e1f568329
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 01:53:45 +0100

    added menus

--- components/Embeds.tsx
+++ components/Embeds.tsx
@@ -1,7 +1,37 @@
+import { Menu } from "@headlessui/react";
 import React from "react";
 
 const Embeds = () => {
-  return <div>Embeds</div>;
+  return (
+    <Menu>
+      <Menu.Button>More</Menu.Button>
+      <Menu.Items>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${active && "bg-blue-500"}`}
+              href="/account-settings"
+            >
+              Account settings
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              className={`${active && "bg-blue-500"}`}
+              href="/account-settings"
+            >
+              Documentation
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item disabled>
+          <span className="opacity-75">Invite a friend (coming soon!)</span>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
+  );
 };
 
 export default Embeds;
