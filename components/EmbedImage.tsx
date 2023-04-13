f1cd0d85a12d75e51f42881267aee3b99327477b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 03:10:08 +0100

    close

--- components/EmbedImage.tsx
+++ components/EmbedImage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
 import useImageEmbedModal from "@/stores/useImageEmbedModal";
+import { MdClose } from "react-icons/md";
 
 const EmbedImage = () => {
   const { isOpen, close } = useImageEmbedModal();
@@ -11,7 +12,14 @@ const EmbedImage = () => {
 
   return (
     <Modal isOpen={isOpen} onClose={handleCloseModal}>
-      <h1>Embed image</h1>
+      <div className="bg-slate-200 w-[550px]">
+        <div className="flex justify-between items-center p-4">
+          <h3>Upload Image</h3>
+          <button onClick={handleCloseModal}>
+            <MdClose />
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 };
