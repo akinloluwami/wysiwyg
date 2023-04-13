745a06a213e152464ce8794d5047d769431aed98
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 03:25:13 +0100

    select

--- components/EmbedImage.tsx
+++ components/EmbedImage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import Modal from "./Modal";
 import useImageEmbedModal from "@/stores/useImageEmbedModal";
 import { MdClose } from "react-icons/md";
@@ -10,17 +10,27 @@ const EmbedImage = () => {
     close();
   };
 
+  const fileRef = createRef<HTMLInputElement>();
+
+  const handleImageSelect = () => {
+    fileRef.current?.click();
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={handleCloseModal}>
       <div className="bg-white w-[550px] p-5">
-        <div className="flex justify-between items-center p-4">
+        <div className="flex justify-between items-center mb-4">
           <h3>Upload Image</h3>
           <button onClick={handleCloseModal}>
             <MdClose />
           </button>
         </div>
         <div className="w-full h-[150px] border-dashed border-green-400 border-2 flex items-center justify-center">
-          <button className="border border-green-400 p-2 bg-gray-200">
+          <input type="file" ref={fileRef} />
+          <button
+            className="border border-green-400 p-2 bg-gray-200"
+            onClick={handleImageSelect}
+          >
             Upload from device
           </button>
         </div>
