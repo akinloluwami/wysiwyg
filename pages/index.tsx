a10a0e4eee6f9db11eb0c327c282311fe8267e37
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-13 18:29:12 +0100

    test twitter embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Editor from "@/components/Editor";
 import EmbedImage from "@/components/EmbedImage";
 import React, { useState } from "react";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const Index = () => {
   return (
     <div className="flex flex-col w-[50%] mx-auto">
+      <TwitterTweetEmbed tweetId="1609348650395648000" />
+
       <input
         type="text"
         placeholder="Post title"
