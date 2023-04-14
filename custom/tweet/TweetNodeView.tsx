7e0908bac90b2bec741fdda66d04585e43c3133e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-14 18:15:20 +0100

    ___

--- custom/tweet/TweetNodeView.tsx
+++
@@ -1,9 +0,0 @@
-import { NodeView } from "@tiptap/core";
-import { tweetNodeViewRenderer } from "./TweetNodeViewRenderer";
-
-export default class TweetNodeView extends NodeView {
-  mount() {
-    super.mount();
-    tweetNodeViewRenderer(this);
-  }
-}
