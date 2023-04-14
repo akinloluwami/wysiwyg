8df1fb12a1d18a14d0c8551dde121dd5bfb1b0af
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-14 21:03:32 +0100

    removed unused elemnets

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,37 +1,14 @@
-import { useState } from "react";
-
 interface ModalProps {
   isOpen: boolean;
-  onClose: () => void;
   children: React.ReactNode;
 }
 
-const Modal = ({ isOpen, onClose, children }: ModalProps) => {
-  const [modalClasses, setModalClasses] = useState<string>(
-    "opacity-0 pointer-events-none transition ease-in-out duration-500"
-  );
-
-  const openModal = () => {
-    setModalClasses(
-      "opacity-100 pointer-events-auto transition ease-in-out duration-500"
-    );
-  };
-
-  const closeModal = () => {
-    setModalClasses(
-      "opacity-0 pointer-events-none transition ease-in-out duration-500"
-    );
-    onClose();
-  };
-
+const Modal = ({ isOpen, children }: ModalProps) => {
   return (
     <>
       {isOpen && (
-        <div
-          className="fixed inset-0 z-50 bg-gray-900 bg-opacity-50 flex items-center justify-center"
-          onClick={closeModal}
-        >
-          <div onClick={(e) => e.stopPropagation()}>{children}</div>
+        <div className="fixed inset-0 z-50 bg-gray-900 bg-opacity-50 flex items-center justify-center">
+          <div>{children}</div>
         </div>
       )}
     </>
