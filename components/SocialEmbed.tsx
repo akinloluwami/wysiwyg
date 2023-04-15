39dc8e34b5e06e03737aa11908a2a9f9a2337bf5
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-15 13:17:05 +0100

    ??

--- components/SocialEmbed.tsx
+++ components/SocialEmbed.tsx
@@ -1,75 +1,72 @@
 import React, { useState, createRef } from "react";
 import Modal from "./Modal";
-import useImageEmbedModal from "@/stores/useImageEmbedModal";
+import useSocialEmbedModal from "@/stores/useSocialEmbedModal";
 import { MdClose } from "react-icons/md";
 
 const EmbedSocial = ({ editor }: any) => {
-  const { isImageModalOpen, closeImageModal } = useImageEmbedModal();
+  const { isSocialModalOpen, closeSocialModal } = useSocialEmbedModal();
 
-  const handleCloseModal = () => {
-    closeImageModal();
-  };
-
-  const fileRef = createRef<HTMLInputElement>();
-
-  const handleImageSelect = () => {
-    fileRef.current?.click();
-  };
-
-  const [file, setFile] = useState(null);
-  const handleImageUpload = () => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const image = new Image();
-        image.src = reader.result as string;
-        editor.chain().focus().setImage({ src: image.src }).run();
-      };
-      reader.readAsDataURL(file);
-      setFile(null);
-    }
+  const handleClose = () => {
+    closeSocialModal();
   };
 
   return (
-    <Modal isOpen={isImageModalOpen}>
+    <Modal isOpen={isSocialModalOpen}>
       <div className="bg-white w-[550px] p-5">
         <div className="flex justify-between items-center mb-4">
-          <h3>Upload Image</h3>
-          <button onClick={handleCloseModal}>
+          <h3>Embed Video</h3>
+          <button onClick={handleClose}>
             <MdClose />
           </button>
         </div>
-        <div className="w-full h-[150px] border-dashed border-green-400 border-2 flex items-center justify-center">
-          <input
-            type="file"
-            ref={fileRef}
-            className="hidden"
-            onChange={(e: any) => setFile(e.target.files[0])}
-          />
-          <button
-            className="border border-green-400 p-2 bg-gray-200"
-            onClick={handleImageSelect}
+        <div className="">
+          <select
+            className="w-full border-2 border-gray-300 p-2 mt-3 mb-5"
+            onChange={(e) => setPlatform(e.target.value)}
           >
-            Upload from device
-          </button>
-        </div>
-        <div className="flex gap-3 mt-5">
-          <button
-            className="bg-green-400 py-2 px-8 rounded-sm text-white disabled:opacity-50 disabled:cursor-not-allowed"
-            onClick={() => {
-              handleImageUpload();
-              closeImageModal();
-            }}
-            disabled={!file}
-          >
-            Embed
-          </button>
-          <button
-            className="py-2 px-8 rounded-sm bg-gray-200"
-            onClick={closeImageModal}
-          >
-            Cancel
-          </button>
+            <option value="YouTube">YouTube</option>
+            <option value="Vimeo">Vimeo</option>
+          </select>
+          {platform === "YouTube" && (
+            <input
+              type="text"
+              placeholder="Enter URL"
+              className="w-full border-2 border-gray-300 p-2 mt-3"
+              onChange={(e) => setVideoUrl(e.target.value)}
+              autoFocus={true}
+              value={videoUrl.trim()}
+            />
+          )}
+          {platform === "Vimeo" && (
+            <textarea
+              autoFocus={true}
+              placeholder="Paste Vimeo iframe code"
+              className="w-full"
+              onChange={(e) => setVimeoCode(e.target.value)}
+              value={vimeoCode}
+            ></textarea>
+          )}
+          <div className="flex gap-3 mt-5">
+            <button
+              className="bg-green-400 py-2 px-8 rounded-sm text-white disabled:opacity-50 disabled:cursor-not-allowed"
+              onClick={() => {
+                embedVideo();
+                // closeVideoModal();
+              }}
+              disabled={
+                (platform === "YouTube" && !videoUrl) ||
+                (platform === "Vimeo" && !vimeoCode)
+              }
+            >
+              Embed
+            </button>
+            <button
+              className="py-2 px-8 rounded-sm bg-gray-200"
+              onClick={handleClose}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </Modal>
