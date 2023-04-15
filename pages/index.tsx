1494f748c9e0a4aeff2aa630e1497625d762fa58
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-15 13:10:34 +0100

    removed blockquote

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,6 @@ import Editor from "@/components/Editor";
 const Index = () => {
   return (
     <div className="flex flex-col w-[50%] mx-auto">
-      <blockquote className="twitter-tweet">
-        <p lang="en" dir="ltr">
-          Hey{" "}
-          <a href="https://twitter.com/FarzaTV?ref_src=twsrc%5Etfw">@FarzaTV</a>{" "}
-          I gtfol last night. 🎉
-        </p>
-        &mdash; Akinkunmi ♦ N&amp;W s3 (@xing0x){" "}
-        <a href="https://twitter.com/xing0x/status/1646262950267695105?ref_src=twsrc%5Etfw">
-          April 12, 2023
-        </a>
-      </blockquote>
       <input
         type="text"
         placeholder="Post title"
